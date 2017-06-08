3a2c6daf009bdb8b7e70dcb5423d24c7a58ba42e
cohyd <mca.lakshman@gmail.com>
2017-06-08 16:55:04 +0530

    Added Web Application in Public GitHub
    Added Web Application in Public GitHub

---
+++ WebApplication/ContosoClinic/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace ContosoWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
